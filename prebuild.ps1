8cbd758ae7b031af4f7447d02faac84486bf5c11
Jan Pieter Posthuma <jppp@surfacebook.jppp.org>
2019-08-27 21:42:46 +0200

    removing braches

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,1 +1,1 @@
-(Get-Content "($env:Build_Repository_LocalPath)\theme\partials\head.tmpl.partial").replace('[BuildNumber]', '($env:Build_BuildNumber)') | Set-Content "($env:Build_Repository_LocalPath)\theme\partials\head.tmpl.partial"
+(Get-Content "$env:Build_Repository_LocalPath\theme\partials\head.tmpl.partial").replace('[BuildNumber]', '$env:Build_BuildNumber') | Set-Content "$env:Build_Repository_LocalPath\theme\partials\head.tmpl.partial"
